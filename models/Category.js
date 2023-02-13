8671b1ce388981509d8bc2c352f7c6cf183a0faf
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-02-13 08:26:49 -0500

    first model

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,12 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTERGER,
+      allowNull: false,
+      primaryKey: true, 
+      autoIncrement: true
+    }
   },
   {
     sequelize,
