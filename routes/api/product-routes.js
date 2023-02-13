ceba2d096232f61805a99fd36051f02464c4eecc
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-02-13 09:55:24 -0500

    added error coding

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,10 +8,23 @@ router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   Product.findAll({
-
+    attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name']
+      }
+    ]
   })
-  .then()
-  .catch()
+    .then()
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 })
 
 
@@ -20,23 +33,31 @@ router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   Product.findOne({
-
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name']
+      }
+    ]
   })
   .then()
-  .catch()
-})
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
 
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
