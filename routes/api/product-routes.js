9f0677c0a9696fbe9678bd6b2c508314e3e90d0e
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-02-13 09:40:52 -0500

    started product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,13 +7,23 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-});
+  Product.findAll({
+
+  })
+})
+.then()
+.catch()
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-});
+  Product.findOne({
+
+  })
+})
+.then()
+.catch()
 
 // create new product
 router.post('/', (req, res) => {
@@ -91,6 +101,11 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
-});
+  Product.destroy({
+
+  })
+})
+.then()
+.catch()
 
 module.exports = router;
